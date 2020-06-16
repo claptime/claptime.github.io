c921b8be89238da4c4dea7b5b5253211a215b24b
Christophe Bougère <christophe.bougere@gmail.com>
2020-06-16 13:48:09 +0200

    Updates

---
+++ precache-manifest.e277c589c063a1b9fc2233a376ccadf6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccd6b209b18b4443902cb60dcdd30c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56756cf2f0e775cdc13d",
+    "url": "/static/css/2.e5e999d7.chunk.css"
+  },
+  {
+    "revision": "56756cf2f0e775cdc13d",
+    "url": "/static/js/2.b16ed4db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b16ed4db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a011e4ed6e7a7a92204e",
+    "url": "/static/js/main.7d51be3d.chunk.js"
+  },
+  {
+    "revision": "64514b0ea0ef364026d8",
+    "url": "/static/js/runtime-main.cbdc9ba5.js"
+  },
+  {
+    "revision": "9dd55f851464e8a0f9664275814ceea4",
+    "url": "/static/media/background-landing.9dd55f85.svg"
+  },
+  {
+    "revision": "487f9d53bae70db312d37f6e41eb919e",
+    "url": "/static/media/background-regular.487f9d53.svg"
+  },
+  {
+    "revision": "72217712eb8d28872e7069322f3fda23",
+    "url": "/static/media/source-sans-pro-italic.72217712.eot"
+  },
+  {
+    "revision": "8256cfd7e4017a7690814879409212cd",
+    "url": "/static/media/source-sans-pro-italic.8256cfd7.ttf"
+  },
+  {
+    "revision": "e74f0128884561828ce8c9cf5c284ab8",
+    "url": "/static/media/source-sans-pro-italic.e74f0128.woff"
+  },
+  {
+    "revision": "1d71438462d532b62b05cdd7e6d7197d",
+    "url": "/static/media/source-sans-pro-regular.1d714384.eot"
+  },
+  {
+    "revision": "2da39ecf9246383937da11b44b7bd9b4",
+    "url": "/static/media/source-sans-pro-regular.2da39ecf.ttf"
+  },
+  {
+    "revision": "e7acc589bb558fe58936a853f570193c",
+    "url": "/static/media/source-sans-pro-regular.e7acc589.woff"
+  },
+  {
+    "revision": "0f3da1edf1b5c6a94a6ad948a7664451",
+    "url": "/static/media/source-sans-pro-semibold.0f3da1ed.eot"
+  },
+  {
+    "revision": "1cb8e94f1185f1131a0c895165998f2b",
+    "url": "/static/media/source-sans-pro-semibold.1cb8e94f.woff"
+  },
+  {
+    "revision": "f3565095e6c9158140444970f5a2c5ed",
+    "url": "/static/media/source-sans-pro-semibold.f3565095.ttf"
+  },
+  {
+    "revision": "58153ac7194e141d1e73ea88c6b63861",
+    "url": "/static/media/source-sans-pro-semibolditalic.58153ac7.eot"
+  },
+  {
+    "revision": "6b058fc2634b01d837c3432316c3141f",
+    "url": "/static/media/source-sans-pro-semibolditalic.6b058fc2.woff"
+  },
+  {
+    "revision": "c7e698a4d0956f4a939f42a05685bbf5",
+    "url": "/static/media/source-sans-pro-semibolditalic.c7e698a4.ttf"
+  }
+]);
